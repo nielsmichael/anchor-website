2081904a934f296c649fb67f9f32b2ada6d94437
xXnielsXx <nielsmichael@comcast.net>
2022-06-10 16:43:14 +0900

    Configure tailwindcss, postcss, autoprefixer

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{html,js,ts,tsx,jsx}", 
+    "./components/**/*.{html,js,ts,tsx,jsx}"
+  ],
+  theme: {
+    extend: {
+      height: {
+        'half-screen': '50vh'
+      }
+    }
+  },
+  plugins: []
+}
