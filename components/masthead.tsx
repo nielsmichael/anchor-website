23bf124a6ff5cf7e13f1ccff78ab6d3f4591abce
xXnielsXx <nielsmichael@comcast.net>
2022-06-14 10:15:02 +0900

    Add bg image, logo to masthead

--- components/masthead.tsx
+++ components/masthead.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
-import Image from 'next/image';
+import React from 'react'
+import Image from 'next/image'
 
 const Masthead: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <div className="p-12 z-10 font-bold text-center">
-        <h1 className="mb-6 text-5xl xl:text-6xl">Anchor Investments</h1>
+      <Image
+        src="/assets/img/City-Buildings.jpg"
+        className="absolute w-full h-full object-cover"
+        layout="fill"
+      />
+      <div className={`flex-grow-0 pt-10 transition-opacity duration-1000`}>
+        <Image
+          src="/assets/favicon-anchor.ico"
+          width={114 / 3}
+          height={114 / 3}
+          alt="logo"
+        />
+      </div>
+      <div className="p-12 z-10 font-bold text-center justify-center items-center text-white drop-shadow-[0_5px_3px_rgba(0,0,0,0.5)] flex-1 flex flex-col">
+        <h1 className="mb-6 text-4xl xl:text-6xl">Anchor Investments</h1>
         <h2 className="mb-3 text-2xl xl:text-3xl tracking-tight">
           build your future.
         </h2>
@@ -15,4 +28,4 @@ const Masthead: React.FC = () => {
   );
 }
 
-export default Masthead;
+export default Masthead
