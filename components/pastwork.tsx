361871c9c78f900f8b8349fa67970de4aa969cc3
xXnielsXx <nielsmichael@comcast.net>
2022-06-16 11:30:59 +0900

    fix display issue in pastwork.tsx

--- components/pastwork.tsx
+++ components/pastwork.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { TileBackground, TileContent, TileWrapper } from "./tile";
 
-const PastWork = () => {
-  <TileWrapper pageCount={3}>
+const PastWork = () => (
+  <TileWrapper numOfPages={3}>
     <TileBackground></TileBackground>
     <TileContent>Test</TileContent>
-    <TileContent>Test 2</TileContent>
   </TileWrapper>
-}
+)
 
 export default PastWork
--- components/tile.tsx
+++ components/tile.tsx
@@ -2,28 +2,27 @@ import React, { useRef, useContext } from 'react'
 import { ScrollContext } from '../utils/parallax-scroll'
 
 interface WrapperProps {
-  pageCount: number
+  numOfPages: number
 }
 
-interface TileCtxVal {
-  pageCount: number
+interface TileContextValue {
+  numOfPages: number
   currentPage: number
 }
 
-export const TileContext = React.createContext<TileCtxVal>({
-  pageCount: 0,
+export const TileContext = React.createContext<TileContextValue>({
+  numOfPages: 0,
   currentPage: 0
 })
 
 export const TileWrapper: React.FC<WrapperProps> = ({
-  // children,
-  pageCount
+  children,
+  numOfPages
 }) => {
   const { scrollY } = useContext(ScrollContext)
   const refContainer = useRef<HTMLDivElement>(null)
+  let currentPage = 0;
   const { current: elContainer } = refContainer
-
-  let currentPage = 0
 
   // Use similar process as in services.tsx to calculate scroll position
   if (elContainer) {
@@ -31,11 +30,11 @@ export const TileWrapper: React.FC<WrapperProps> = ({
     const screenHeight = window.innerHeight
     const halfHeight = screenHeight / 2
     const percentY = Math.min(clientHeight + halfHeight, Math.max(-screenHeight, scrollY - offsetTop) + halfHeight) / clientHeight
-    pageCount = percentY * pageCount
+    numOfPages = percentY * numOfPages
   }
 
   return (
-    <TileContext.Provider value={{ pageCount, currentPage }}>
+    <TileContext.Provider value={{ numOfPages, currentPage }}>
       <div
         ref={refContainer}
         className="relative bg-anchor-black text-anchor-white"
@@ -43,7 +42,7 @@ export const TileWrapper: React.FC<WrapperProps> = ({
         {children}
       </div>
     </TileContext.Provider>
-  )
+  );
 }
 
 export const TileBackground: React.FC = ({ children }) => (
